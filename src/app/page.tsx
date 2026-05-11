7726396d87515403347161ad3636bab25c15ba67
CORELAB ACADEMY <corelabacademy@gmail.com>
2026-05-11 15:46:22 +0700

    use html homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,25 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main
+    <iframe
+      src="/index.html"
       style={{
-        background: "#000",
-        color: "#fff",
-        minHeight: "100vh",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        fontFamily: "sans-serif",
+        width: "100%",
+        height: "100vh",
+        border: "none",
       }}
-    >
-      <h1 style={{ fontSize: "64px", fontWeight: "bold" }}>
-        CORELAB ACADEMY
-      </h1>
-
-      <p style={{ marginTop: "20px", fontSize: "24px", opacity: 0.7 }}>
-        Future Learning Platform
-      </p>
-    </main>
+    />
   );
 }
